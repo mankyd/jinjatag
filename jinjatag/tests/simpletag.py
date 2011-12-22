ffb851f1acd32da85a6155205f3c11c7e4e4c0ee
Michael Axiak <mike@axiak.net>
2011-12-22 12:51:18 -0500

    Added test for bound methods.

--- jinjatag/tests/simpletag.py
+++ jinjatag/tests/simpletag.py
@@ -9,6 +9,15 @@ class SimpleTagTestCase(JinjaTagTestCase):
     def st_simple_tag(x, y):
         return str(x - y)
 
+    def bound_simple_tag(self, x, y):
+        return str(self) + str(x)
+
+    def test_bound(self):
+        jinjatag.simple_tag()(self.bound_simple_tag)
+        tmpl = self.env.from_string(''' {% bound_simple_tag x=4 y=6 %} ''')
+        self.assertEquals(tmpl.render(), ' test_bound (jinjatag.tests.simpletag.SimpleTagTestCase)4 ')
+
+
     def test_simple(self):
         tmpl = self.env.from_string(''' {% st_simple_tag x=4 y=6 %} ''')
         self.assertEquals(tmpl.render(), ' -2 ')
